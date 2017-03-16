d3f4489d2e6649f9658470153c22496aa71962e7
Melissa Yu <melissa.y00@gmail.com>
2017-03-15 23:59:20 -0700

    First Commit of Book Search Week 2 lab

---
+++ app/src/main/java/com/codepath/android/mybooksearch/models/Book.java
@@ -0,0 +1,139 @@
+package com.codepath.android.mybooksearch.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Book implements Parcelable {
+    private String openLibraryId;
+    private String author;
+    private String title;
+
+    public String getOpenLibraryId() {
+        return openLibraryId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // Get book cover from covers API
+    public String getCoverUrl() {
+        return "http://covers.openlibrary.org/b/olid/" + openLibraryId + "-L.jpg?default=false";
+    }
+    private Book() {
+
+    }
+
+    private Book(Parcel in) {
+        this.title = in.readString();
+        this.openLibraryId = in.readString();
+        this.author = in.readString();
+//        mName = in.readString();
+//        mInfo = in.readParcelable(MySubParcelable.class.getClassLoader());
+    }
+
+    // Returns a Book given the expected JSON
+    public static Book fromJson(JSONObject jsonObject) {
+        Book book = new Book();
+        try {
+            // Deserialize json into object fields
+            // Check if a cover edition is available
+            if (jsonObject.has("cover_edition_key")) {
+                book.openLibraryId = jsonObject.getString("cover_edition_key");
+            } else if(jsonObject.has("edition_key")) {
+                final JSONArray ids = jsonObject.getJSONArray("edition_key");
+                book.openLibraryId = ids.getString(0);
+            }
+            book.title = jsonObject.has("title_suggest") ? jsonObject.getString("title_suggest") : "";
+            book.author = getAuthor(jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // Return new object
+        return book;
+    }
+
+    // Return comma separated author list when there is more than one author
+    private static String getAuthor(final JSONObject jsonObject) {
+        try {
+            final JSONArray authors = jsonObject.getJSONArray("author_name");
+            int numAuthors = authors.length();
+            final String[] authorStrings = new String[numAuthors];
+            for (int i = 0; i < numAuthors; ++i) {
+                authorStrings[i] = authors.getString(i);
+            }
+            return TextUtils.join(", ", authorStrings);
+        } catch (JSONException e) {
+            return "";
+        }
+    }
+
+    // Decodes array of book json results into business model objects
+    public static ArrayList<Book> fromJson(JSONArray jsonArray) {
+        ArrayList<Book> books = new ArrayList<>(jsonArray.length());
+        // Process each result in json array, decode and convert to business
+        // object
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject bookJson;
+            try {
+                bookJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+            Book book = Book.fromJson(bookJson);
+            if (book != null) {
+                books.add(book);
+            }
+        }
+        return books;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+
+        out.writeString(title);
+        out.writeString(openLibraryId);
+        out.writeString(author);
+
+    }
+
+    // After implementing the `Parcelable` interface, we need to create the
+    // `Parcelable.Creator<MyParcelable> CREATOR` constant for our class;
+    // Notice how it has our class specified as its type.
+    public static final Parcelable.Creator<Book> CREATOR
+            = new Parcelable.Creator<Book>() {
+
+        // This simply calls our new constructor (typically private) and
+        // passes along the unmarshalled `Parcel`, and then returns the new object!
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        // We just need to copy this and change the type to match our class.
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+
+}
